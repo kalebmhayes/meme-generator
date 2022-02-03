5202a033c7998ac1136595863fe10040f9d77b7f
kalebmhayes <khayespaperless@gmail.com>
2022-02-02 21:08:37 -0500

    first

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <div className='header-div'>
+            <img className='header-img' src='images/troll-face.svg'/>
+            <h1 className='header-title'>Meme Generator</h1>
+            <h3 className='header-info'>React Project 4</h3>
+        </div>
+    )
+}
+
+export default Header
