18b9debe9c5e8bb2caaf6bd1a21c6737c63a4871
kalebmhayes <khayespaperless@gmail.com>
2022-02-02 21:22:21 -0500

    meme updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './index.css'
 import Header from './Components/Header.jsx'
+import Meme from './Components/Meme.jsx'
 
 
 function App() {
   return (
     <div>
       <Header />
+      <Meme />
     </div>
+
   );
 }
 
